168047d23b03c490756e46bf2ef41999cc78bdf0
Ashish Majnekar <firebeast18@gmail.com>
2022-07-27 16:02:22 +0530

    update package

--- src/app.js
+++ src/app.js
@@ -57,6 +57,7 @@ app.get('/index', (req, res) => {
 // v1 api routes
 app.use('/v1', routes);
 
+
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
